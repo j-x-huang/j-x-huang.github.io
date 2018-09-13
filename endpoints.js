936c15ca6d851a6d48a2f3e9a0f33b0a85d8ed53
Jack Huang <huangxjack@gmail.com>
2018-09-13 14:03:56 +1200

    Update endpoints.js

--- endpoints.js
+++ endpoints.js
@@ -1,5 +1,5 @@
-const ADMIN_ENDPOINT = "http://localhost:3002/api/"
+const ADMIN_ENDPOINT = "http://172.24.41.165:3002/api/"
 
-const PATIENT_ENDPOINT = "http://localhost:3000/api/"
+const PATIENT_ENDPOINT = "http://172.24.41.165:3000/api/"
 
-const HP_ENDPOINT = "http://localhost:3001/api/"
+const HP_ENDPOINT = "http://172.24.41.165:3001/api/"
